2ab696cc30dc78adc358ec121ded1f58d3247972
NagyFelixNF <huntertelo25@gmail.com>
2021-12-06 17:00:34 +0100

    small things fixed

--- weddingappangular/src/app/nav/nav.component.ts
+++ weddingappangular/src/app/nav/nav.component.ts
@@ -10,6 +10,7 @@ import { AccountService } from '../services/account.service';
 })
 export class NavComponent implements OnInit {
   model:any = {}
+  usern:any;
 
   constructor(public accountService: AccountService) { }
 
@@ -17,9 +18,9 @@ export class NavComponent implements OnInit {
   }
 
   login():void{
-    console.log("anyas");
       this.accountService.login(this.model).subscribe(response => {
         console.log(response);
+        this.usern = response.username;
       }, error =>{
         console.log(error);
       });
